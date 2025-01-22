d676b98dac4f4961d61030f00cc3167c62f2b512
Bohdan Chernykh <dev@bchernykh.anonaddy.com>
2025-01-22 19:12:47 +0200

    feat: implement analyzer

---
+++ analyzer.py
@@ -0,0 +1,104 @@
+from tokenizer import Token, TokenType
+
+VALID_START_OF_EXPRESSION = {
+    TokenType.IDENTIFIER,
+    TokenType.NUMBER,
+    TokenType.MINUS_SIGN,
+    TokenType.OPENING_PARENTHESIS
+}
+
+VALID_END_OF_EXPRESSION = {
+    TokenType.IDENTIFIER,
+    TokenType.NUMBER,
+    TokenType.CLOSING_PARENTHESIS
+}
+
+OPERATORS = {
+    TokenType.ADDITION_OPERATOR,
+    TokenType.MINUS_SIGN,
+    TokenType.MULTIPLICATION_OPERATOR,
+    TokenType.DIVISION_OPERATOR
+}
+
+FOLLOWS_OPERAND = {*OPERATORS, TokenType.CLOSING_PARENTHESIS}
+FOLLOWS_OPERATOR = {
+    TokenType.IDENTIFIER,
+    TokenType.NUMBER,
+    TokenType.OPENING_PARENTHESIS
+}
+
+VALID_FOLLOW_SETS = {
+    TokenType.IDENTIFIER: FOLLOWS_OPERAND,
+    TokenType.NUMBER: FOLLOWS_OPERAND,
+    TokenType.ADDITION_OPERATOR: FOLLOWS_OPERATOR,
+    TokenType.MINUS_SIGN: FOLLOWS_OPERATOR,
+    TokenType.MULTIPLICATION_OPERATOR: FOLLOWS_OPERATOR,
+    TokenType.DIVISION_OPERATOR: FOLLOWS_OPERATOR,
+    TokenType.OPENING_PARENTHESIS: {*FOLLOWS_OPERATOR, TokenType.MINUS_SIGN},
+    TokenType.CLOSING_PARENTHESIS: {*FOLLOWS_OPERAND}
+}
+
+
+class SyntaxAnalysisError(Exception):
+    """Invalid syntax encountered."""
+
+
+def analyze(tokens: list[Token]) -> list[SyntaxAnalysisError]:
+    return SyntaxAnalyzer(tokens).analyze()
+
+
+class SyntaxAnalyzer:
+    def __init__(self, tokens: list[Token]) -> None:
+        self.tokens = tokens
+        self.errors: list[SyntaxAnalysisError] = []
+
+    def analyze(self) -> list[SyntaxAnalysisError]:
+        self._analyze_start_of_expression()
+        self._analyze_following_tokens()
+        self._analyze_end_of_expression()
+        self._analyze_parentheses_usage()
+        return self.errors
+
+    def _error(self, message: str) -> None:
+        self.errors.append(SyntaxAnalysisError(message))
+
+    def _analyze_start_of_expression(self) -> None:
+        token = self.tokens[0]
+        if token.type not in VALID_START_OF_EXPRESSION:
+            self._error(f"Expression can't start with {token.type!r}")
+
+    def _analyze_end_of_expression(self) -> None:
+        token = self.tokens[-1]
+        if token.type not in VALID_END_OF_EXPRESSION:
+            self._error(f"Expression can't end with {token.type!r}")
+
+    def _analyze_following_tokens(self) -> None:
+        previous = None
+        for token in self.tokens:
+            if previous and token.type not in VALID_FOLLOW_SETS[previous.type]:
+                self._error(
+                    f"{token.position}: {previous.type!r} can't be followed "
+                    f"by {token.type!r}"
+                )
+            previous = token
+
+    def _analyze_parentheses_usage(self) -> None:
+        stack: list[Token] = []
+        for token in self.tokens:
+            if token.type == TokenType.OPENING_PARENTHESIS:
+                stack.append(token)
+            elif token.type == TokenType.CLOSING_PARENTHESIS:
+                if not stack:
+                    self._error(
+                        f"{token.position}: {TokenType.CLOSING_PARENTHESIS!r} "
+                        "has never been openned with "
+                        f"{TokenType.OPENING_PARENTHESIS!r}"
+                    )
+                else:
+                    stack.pop()
+
+        for token in stack:
+            self._error(
+                f"{token.position}: {TokenType.OPENING_PARENTHESIS!r} "
+                f"has never been closed with {TokenType.CLOSING_PARENTHESIS!r}"
+            )
---
+++ test_analyzer.py
@@ -0,0 +1,142 @@
+import itertools as it
+from unittest import TestCase
+
+from analyzer import analyze
+from test_tokenizer import parametrize
+from tokenizer import TokenType, tokenize
+
+OPERATION_TO_TOKEN_TYPE = {
+    "+": TokenType.ADDITION_OPERATOR,
+    "-": TokenType.MINUS_SIGN,
+    "*": TokenType.MULTIPLICATION_OPERATOR,
+    "/": TokenType.DIVISION_OPERATOR,
+}
+
+EMPTY_PARENTHESES_ERROR_MSG = (
+    f"{TokenType.OPENING_PARENTHESIS!r} can't be followed by "
+    f"{TokenType.CLOSING_PARENTHESIS!r}"
+)
+CLOSING_PARENTHESIS_ERROR_MSG = (
+    f"{TokenType.CLOSING_PARENTHESIS!r} has never been openned with "
+    f"{TokenType.OPENING_PARENTHESIS!r}"
+)
+OPENING_PARENTHESIS_ERROR_MSG = (
+    f"{TokenType.OPENING_PARENTHESIS!r} has never been closed with "
+    f"{TokenType.CLOSING_PARENTHESIS!r}"
+)
+
+class TestAnalyzer(TestCase):
+    def test_expression_can_not_start_with_closing_parenthesis(self) -> None:
+        tokens, _ = tokenize(")a+b")
+        errors = analyze(tokens)
+        self.assertTrue(str(TokenType.CLOSING_PARENTHESIS) in str(errors[0]))
+
+    @parametrize(
+        "expression,operator", [
+            (f"{op}a+b", OPERATION_TO_TOKEN_TYPE[op])
+            for op in '+*/'
+        ]
+    )
+    def test_expression_can_not_start_with_algebraic_operations(
+        self, expression: str, operator: TokenType
+    ) -> None:
+        # Well, minus sign is allowed as unary operation
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(operator) in str(errors[0]))
+
+    @parametrize(
+        "expression,operator", [
+            (f"a+b{op}", OPERATION_TO_TOKEN_TYPE[op])
+            for op in '+-*/'
+        ]
+    )
+    def test_expression_can_not_end_with_algebraic_operations(
+        self, expression: str, operator: TokenType
+    ) -> None:
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(operator) in str(errors[0]))
+
+    @parametrize(
+        "expression,operator", [
+            (f"a{op1}{op2}b", OPERATION_TO_TOKEN_TYPE[op2])
+            for op1, op2 in it.product("+-*/", repeat=2)
+        ]
+    )
+    def test_expression_can_not_contain_double_algebraic_operations(
+        self, expression: str, operator: TokenType
+    ) -> None:
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(operator) in str(errors[0]))
+
+    @parametrize(
+        "expression,last_valid_token_type", [
+            ("a+b(c-d)", TokenType.IDENTIFIER),
+            ("a+4.31(c-d)", TokenType.NUMBER),
+        ]
+    )
+    def test_expression_must_contain_operations_before_parentheses(
+        self, expression: str, last_valid_token_type: TokenType
+    ) -> None:
+        # Except for start of expression, of course
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(last_valid_token_type) in str(errors[0]))
+
+    @parametrize(
+        "expression,operator", [
+            (f"({op}a+b)", OPERATION_TO_TOKEN_TYPE[op])
+            for op in "+*/"
+        ]
+    )
+    def test_expression_can_not_contain_operations_right_after_opening_parenthesis(
+        self, expression: str, operator: TokenType
+    ) -> None:
+        # Except for unary minus
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(operator) in str(errors[0]))
+
+    @parametrize(
+        "expression,operator", [
+            (f"(a+b){op}", OPERATION_TO_TOKEN_TYPE[op])
+            for op in "+-*/"
+        ]
+    )
+    def test_end_of_expression_can_not_contain_operations_right_before_closing_parenthesis(
+        self, expression: str, operator: TokenType
+    ) -> None:
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(operator) in str(errors[0]))
+
+    @parametrize(
+        "expression,error_message",
+        [
+            ("(a+b))", CLOSING_PARENTHESIS_ERROR_MSG),
+            ("((a+b)", OPENING_PARENTHESIS_ERROR_MSG)
+        ]
+    )
+    def test_expression_can_not_contain_odd_number_of_parentheses(
+        self, expression: str, error_message: str
+    ) -> None:
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(error_message) in str(errors[0]))
+
+    @parametrize(
+        "expression,error_message",
+        [
+            (expression, EMPTY_PARENTHESES_ERROR_MSG)
+            for expression in ["()", "a+()", "a+(b+())"]
+        ]
+    )
+    def test_expression_can_not_contain_empty_parentheses(
+        self, expression: str, error_message: str
+    ) -> None:
+        tokens, _ = tokenize(expression)
+        errors = analyze(tokens)
+        self.assertTrue(str(error_message) in str(errors[0]))
+
